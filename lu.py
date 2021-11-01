b77c8fe964bb0503110b18ce9b72aed82b2d0fcd
Aluno da Infinity School <aluno@3D.local>
2021-11-01 20:31:13 -0300

    Hello World!

---
+++ lu.py
@@ -0,0 +1,10 @@
+n1 = int(input('informe o primeiro numero:'))
+n2 = int(input('informe o sugundo numero:'))
+
+print('numero 1: ',n1, ' numero 2: ', n2)
+
+aux = n1
+n1 = n2
+n2 = aux
+
+print('numero 1: ',n1, 'numero 2: ', n2)
